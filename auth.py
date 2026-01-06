998fbfc33ba1f6489f9a37e1f7bef9bfe6303fc2
nishantkumar778-bit <nishant.kumar778@chicmicstudios.in>
2026-01-06 09:44:39 +0000

    deleted old files

--- auth.py
+++
@@ -1,9 +0,0 @@
-from passlib.context import CryptContext
-
-pwd = CryptContext(schemes=["bcrypt"])
-
-def hash_password(password):
-    return pwd.hash(password)
-
-def verify_password(password, hash):
-    return pwd.verify(password, hash)
